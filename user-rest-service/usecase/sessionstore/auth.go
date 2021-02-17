033a7f8cb9560cbfcc33bc6d0671fa52e0a37cd3
Yusuke Taira <ney07214545@icloud.com>
2021-02-18 00:03:20 +0900

    Change method name from AddSessionID to StoreLoginInfo

--- user-rest-service/usecase/sessionstore/auth.go
+++ user-rest-service/usecase/sessionstore/auth.go
@@ -3,5 +3,5 @@ package sessionstore
 import "github.com/paypay3/kakeibo-rest-api-ddd/user-rest-service/domain/userdomain"
 
 type SessionStore interface {
-	AddSessionID(sessionID string, userID userdomain.UserID) error
+	StoreLoginInfo(sessionID string, userID userdomain.UserID) error
 }
